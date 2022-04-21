68e3c6687ddb75cc6527b9ebd4ef15912dbf65a8
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-04-21 08:05:51 +0530

    Update logo2.sh

--- OnlineHacking/logo2.sh
+++ OnlineHacking/logo2.sh
@@ -1,5 +1,5 @@
 echo ""
-echo -e "\e[1;96m< < ==================== < Phishing Page > ==================== > >\e[m "
+echo -e "\e[1;96m< < =============== < Phishing Page > =============== > >\e[m "
 printf " \e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m " 
 echo " "
 printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Jio Recharge \e[0m       \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;91;107m Festival \e[0m   "
@@ -8,15 +8,26 @@ printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
 echo
 printf "\e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;37;41m Live Youtube \e[0m       \e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m] \e[0m\e[1;34;103m Online Meeting \e[0m   "
 echo " "
-echo -e "\e[1;96m< < ==================== < Tool Setting > ==================== > >\e[m "
-echo ""
-
-${cyan}[${white}i${cyan}] ${yellow}Change Image Directory (current: ${red}${FOL}${yellow})
-${cyan}[${white}t${cyan}] ${yellow}Change Default Tunneler (current: ${red}${TN}${yellow})
-${cyan}[${white}x${cyan}] ${yellow}About
-${cyan}[${white}m${cyan}] ${yellow}More tools
-${cyan}[${white}0${cyan}] ${yellow}Exit${blue}
-
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"  
 echo " "
-echo -e "\e[1;96m< < < ============================================================ > > >\e[m "
+echo -e "\e[1;96m< < =============== < Tool Setting > =============== > >\e[m "
+echo ""
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+echo ""
+printf "\e[1;31m[\e[0m\e[1;92mi\e[0m\e[1;31m] \e[0m\e[1;33mChange Image Directory (current:\e[0m "
+echo ""
+printf "\e[1;31m[\e[0m\e[1;92mt\e[0m\e[1;31m] \e[0m\e[1;33mChange Default Tunneler (current:\e[0m "
+echo ""
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+echo ""
+printf "\e[1;31m[\e[0m\e[1;92mx\e[0m\e[1;31m] \e[0m\e[1;33mMore tools\e[0m   "
+echo ""
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+echo ""
+printf "\e[1;31m[\e[0m\e[1;92mx\e[0m\e[1;31m] \e[0m\e[1;33mAbout\e[0m                    \e[1;31m[\e[0m\e[1;92m0\e[0m\e[1;31m] \e[0m\e[1;33mExit\e[0m\n"
+echo ""
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+echo ""
+echo -e "\e[1;96m< < =============== < Choose Option > =============== > >\e[m "
 echo ""
